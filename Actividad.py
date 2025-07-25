fff8bd4b5009d5d01b7378764fd57775cc01972c
moyanoignacio <moyanobignacio@gmail.com>
2025-07-25 12:34:09 +0000

    Carga actividad 1

---
+++ Actividad.py
@@ -0,0 +1,17 @@
+# Historia 2
+#La serie sigue la historia de Johnny Lawrence, exestudiante del dojo Cobra Kai,
+# quien busca la redención reabriendo el infame dojo y reavivando, en el proceso,
+# su rivalidad con un ahora exitoso Daniel LaRusso.
+# Este enemigo ha estado luchando por mantener el equilibrio en su vida sin la guía de su mentor, el señor Miyagi.
+# La serie trata sobre dos hombres que enfrentan sus demonios del pasado y
+# resuelven las frustraciones del presente de la única manera en que saben hacerlo: a través del karate.
+
+nombrePrincipal = str(input("Dame el nombre del personaje principal"))
+apellidoPrincipal = str(input("Dame el apellido del personaje principal"))
+dojo =str(input("Dame el nombre del dojo"))
+nombreHeroe = str(input("Dame el nombre del heroe" ")) 
+apellidoHeroe = str(input("Dame el apellido del heroe"))
+
+print("La serie sigye la historia de "+nombrePrincipal+" "+apellidoPrincipal+"exestudiantedel dojo Cobra Kai")
+print("quien busca la redencion reabriendo el infame dojo y reavivando, en el proceso")
+print(f"su rivalidad con un ahora exitoso{nombreHeroe}{apellidoHeroe}")
---
+++ main.py
@@ -0,0 +1,5 @@
+a = 1
+b = 2
+print(a+b)
+c = str(input("Hola! decime tu nombre: ")
+print("Hola "+c)
